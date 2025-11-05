7c37daaadf3fab4068ebd5ec5f27bb7084ff0b7f
Danny <m.r.fouladi7@gmail.com>
2025-11-05 14:19:33 +0330

    Infrastructure ServiceContainer

---
+++ Product.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+using Resource.Share.Lib.DependencyInjection;
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+        {
+             ShareServiceContainer.AddShareService<ProductDbContext>(services, config, config["MySerilog:FirstName"]!);
+
+            services.AddScoped<IProduct, ProductRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastracturePolicies(this IApplicationBuilder app)
+        {
+            ShareServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
