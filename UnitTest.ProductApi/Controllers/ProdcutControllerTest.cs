d1e168b48b9517665b5800ea7b048ce6c49b287c
Danny <m.r.fouladi7@gmail.com>
2025-11-17 15:29:14 +0330

    initial unit test

---
+++ UnitTest.ProductApi/Controllers/ProdcutControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using FakeItEasy;
+using ProductApi.Application.Interfaces;
+using ProductApi.Presentation.Controllers;
+
+namespace UnitTest.ProductApi.Controllers
+{
+    public class ProdcutControllerTest
+    {
+        private readonly IProduct productInterface;
+        private readonly ProductController productController;
+
+        public ProdcutControllerTest()
+        {
+            productInterface = A.Fake<IProduct>();
+            productController = new ProductController(productInterface);
+        }
+    }
+}
