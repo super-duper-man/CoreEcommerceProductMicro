17056d758186864d1090344c98b8e05ebecfeedb
Danny <m.r.fouladi7@gmail.com>
2025-11-18 13:45:15 +0330

    Initial Product Repository Unit Test

---
+++ UnitTest.ProductApi/Repositories/ProductRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Domain.Entities;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+
+namespace UnitTest.ProductApi.Repositories
+{
+    public class ProductRepositoryTest
+    {
+        public readonly ProductDbContext productDbContext;
+        private readonly ProductRepository productRepository;
+
+        public ProductRepositoryTest()
+        {
+            var options = new DbContextOptionsBuilder<ProductDbContext>()
+                .UseInMemoryDatabase(databaseName: "ProductDb_Test").Options;
+
+            productDbContext = new ProductDbContext(options);
+            productRepository = new ProductRepository(productDbContext);
+        }
+
+        [Fact]
+        public async Task CreateProduct_WhenProductAlreadyExists_ShouldReturnErrorReponse()
+        {
+            //Arrange
+            var existingProduct = new Product { Name = "ExistingProduct" };
+            productDbContext.Products.Add(existingProduct);
+            await productDbContext.SaveChangesAsync();
+
+            //Act
+            var result = await productRepository.CreateAsync(existingProduct);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Flag.Should().BeFalse();
+            result.Message.Should().Be("ExistingProduct already exists");
+        }
+    }
+}
